a616814e62774d60bc1559b78abba3891b589a36
NjordJ <nj0rd3841@gmail.com>
2022-11-17 14:35:29 +0500

    Register page

--- app/src/main/java/com/iruda/ecomm/presentation/account/screens/RegisterFragment.kt
+++ app/src/main/java/com/iruda/ecomm/presentation/account/screens/RegisterFragment.kt
@@ -6,38 +6,34 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.iruda.ecomm.databinding.FragmentLoginBinding
+import com.iruda.ecomm.databinding.FragmentRegisterBinding
 
-class LoginFragment : Fragment() {
+class RegisterFragment : Fragment() {
 
-    private var _binding: FragmentLoginBinding? = null
-    private val binding: FragmentLoginBinding
-        get() = _binding ?: throw  RuntimeException("FragmentLoginBinding is null")
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding: FragmentRegisterBinding
+        get() = _binding ?: throw  RuntimeException("FragmentRegisterBinding is null")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonLogin.setOnClickListener {
-
-        }
-
-        binding.buttonSignIn.setOnClickListener {
-            launchRegisterFragment()
+        binding.buttonSignUp.setOnClickListener {
+            backToLoginFragment()
         }
     }
 
-    private fun launchRegisterFragment() {
+    private fun backToLoginFragment() {
         val action =
-            LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+            RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
 
         findNavController().navigate(action)
     }
